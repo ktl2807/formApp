349b32bf9d81fb7296f833d05eede98fc8e6f4dc
Kuei-tsen-liao <sand964@gmail.com>
2022-05-20 18:44:56 -0700

    Initial Commit

---
+++ formApp/formAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  formAppApp.swift
+//  formApp
+//
+//  Created by sand liao on 20/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct formAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
